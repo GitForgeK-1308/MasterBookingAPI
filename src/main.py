b6ac5fe48421c16c54a9db04f2dcd597b174b539
kirill1308 <kirill.k1308@yahoo.com>
2026-07-27 20:31:43 +0600

    Создание базовой структуры проекта

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+from src.masters.router import router as masters_router
+
+
+app = FastAPI()
+
+app.include_router(masters_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
