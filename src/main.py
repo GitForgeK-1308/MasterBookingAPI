16cb2ee0752702121bf478084cf2a9c1c2f5aed2
kirill1308 <kirill.k1308@yahoo.com>
2026-07-31 19:50:41 +0600

    Создал master_services и сделал базовую настройку

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from src.masters.router import router as masters_router
-
+from src.master_services.router import router as service_router
 
 app = FastAPI(title="MasterBooking")
 
 app.include_router(masters_router)
+app.include_router(service_router)
+
 
 app.add_middleware(
     CORSMiddleware,
