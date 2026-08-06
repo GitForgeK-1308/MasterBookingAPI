e42e8d1be9bc24791f727c69e988629093d9e172
kirill1308 <kirill.k1308@yahoo.com>
2026-08-06 16:02:10 +0600

    Добавлен класс UserRepository с методами для получения пользователя по ID и email, а также для создания нового пользователя с использованием асинхронной сессии SQLAlchemy.

---
+++ src/users/repository.py
@@ -0,0 +1,42 @@
+import uuid
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.users.models import User
+
+
+class UserRepository:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def get_by_id(
+        self,
+        user_id: uuid.UUID,
+    ) -> User | None:
+        return await self.session.scalar(
+            select(User).where(
+                User.id == user_id
+            )
+        )
+
+    async def get_by_email(
+        self,
+        email: str,
+    ) -> User | None:
+        return await self.session.scalar(
+            select(User).where(
+                User.email == email
+            )
+        )
+
+    async def create(
+        self,
+        user: User,
+    ) -> User:
+        self.session.add(user)
+
+        await self.session.commit()
+        await self.session.refresh(user)
+
+        return user
