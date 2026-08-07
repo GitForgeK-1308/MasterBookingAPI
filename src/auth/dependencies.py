925da2ca9faa26b8228f80cf9303cf34a3f876fe
kirill1308 <kirill.k1308@yahoo.com>
2026-08-07 09:23:11 +0600

    Добавлена функция decode_access_token для декодирования JWT токенов с обработкой ошибок. Импортирован InvalidTokenError для управления исключениями при неверных токенах.

---
+++ src/auth/dependencies.py
@@ -0,0 +1,49 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jwt.exceptions import InvalidTokenError
+
+from src.auth.token import decode_access_token
+from src.users.exceptions import UserNotFoundError
+from src.users.models import User
+from src.users.service import UserService
+from src.users.dependencies import get_user_service
+
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="auth/login"
+)
+
+
+async def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    service: UserService = Depends(get_user_service),
+) -> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Не удалось подтвердить пользователя!",
+        headers={
+            "WWW-Authenticate": "Bearer",
+        },
+    )
+
+    try:
+        user_id = decode_access_token(token)
+
+    except InvalidTokenError:
+        raise credentials_exception
+
+    try:
+        user = await service.get_user_by_id(
+            user_id
+        )
+
+    except UserNotFoundError:
+        raise credentials_exception
+
+    if not user.is_active:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Аккаунт пользователя отключён!",
+        )
+
+    return user
--- src/auth/token.py
+++ src/auth/token.py
@@ -1,6 +1,6 @@
 import uuid
 from datetime import datetime, timedelta, timezone
-
+from jwt.exceptions import InvalidTokenError
 import jwt
 
 from src.config import settings
@@ -23,3 +23,24 @@ def create_access_token(
         settings.secret_key,
         algorithm=settings.algorithm,
     )
+
+
+def decode_access_token(
+    token: str,
+) -> uuid.UUID:
+    try:
+        payload = jwt.decode(
+            token,
+            settings.secret_key,
+            algorithms=[settings.algorithm],
+        )
+
+        user_id = payload.get("sub")
+
+        if user_id is None:
+            raise InvalidTokenError
+
+        return uuid.UUID(user_id)
+
+    except (InvalidTokenError, ValueError):
+        raise InvalidTokenError
