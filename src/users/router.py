6d5991f656199ad7db8119dcc7dc7019f986ab3d
kirill1308 <kirill.k1308@yahoo.com>
2026-08-07 09:19:09 +0600

    Добавлен маршрут для входа пользователя с обработкой ошибок неверных учетных данных и отключенных аккаунтов. Реализована логика аутентификации с использованием OAuth2 и создание токена доступа.

--- src/users/router.py
+++ src/users/router.py
@@ -1,3 +1,5 @@
+from fastapi.security import OAuth2PasswordRequestForm
+
 from fastapi import (
     APIRouter,
     Depends,
@@ -5,9 +7,15 @@ from fastapi import (
     status,
 )
 
+from src.auth.token import create_access_token
 from src.users.dependencies import get_user_service
-from src.users.exceptions import EmailAlreadyExistsError
-from src.users.schemas import UserRegister, UserResponse
+from src.users.exceptions import (
+    EmailAlreadyExistsError,
+    InactiveUserError,
+    InvalidCredentialsError,
+)
+
+from src.users.schemas import UserRegister, UserResponse, TokenResponse
 from src.users.service import UserService
 
 
@@ -33,4 +41,43 @@ async def register_user(
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
             detail="Пользователь с таким email уже существует!",
+        )
+
+
+@router.post(
+    "/login",
+    response_model=TokenResponse,
+    status_code=status.HTTP_200_OK,
+)
+async def login_user(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    service: UserService = Depends(get_user_service),
+):
+    try:
+        user = await service.authenticate_user(
+            email=form_data.username,
+            password=form_data.password,
+        )
+
+        access_token = create_access_token(
+            user_id=user.id
+        )
+
+        return TokenResponse(
+            access_token=access_token
+        )
+
+    except InvalidCredentialsError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Неверный email или пароль!",
+            headers={
+                "WWW-Authenticate": "Bearer",
+            },
+        )
+
+    except InactiveUserError:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Аккаунт пользователя отключён!",
         )
