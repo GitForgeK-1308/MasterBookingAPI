6112743005c56d0e2c6f851cf80ac2651a2a2391
kirill1308 <kirill.k1308@yahoo.com>
2026-08-07 11:12:09 +0600

    Выполнен рефакторинг get_master_service: добавлен UserRepository и улучшено управление зависимостями. Для большей ясности обновлен код создания экземпляра репозитория.

--- src/masters/dependencies.py
+++ src/masters/dependencies.py
@@ -4,13 +4,17 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.database.session import get_async_session
 from src.masters.repository import MasterRepository
 from src.masters.service import MasterService
+from src.users.repository import UserRepository
 
 
 def get_master_service(
     session: AsyncSession = Depends(get_async_session)
 ) -> MasterService:
 
-    repository = MasterRepository(session)
+    master_repository = MasterRepository(session)
+    user_repository = UserRepository(session)
 
-
-    return MasterService(repository)
+    return MasterService(
+        repository=master_repository,
+        user_repository=user_repository,
+    )
